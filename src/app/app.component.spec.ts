bb2f637a8c14719fabc319245326e963411e7b1a
Tim Petri <wassertim@gmail.com>
2024-10-12 14:49:41 +0000

    add prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, localhost-guide');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, localhost-guide',
+    );
   });
 });
